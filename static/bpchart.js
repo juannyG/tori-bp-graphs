0156d0bac37c42f4fbaa34b460eb6f575ca9bb93
Juan Gutierrez <juanny.gee@gmail.com>
2017-11-19 14:12:42 -0500

    Basic ChartJS scaffold

---
+++ static/bpchart.js
@@ -0,0 +1,20 @@
+new Chart(
+    document.getElementById("myChart"), {
+        "type":"line",
+        "data": {
+            "labels": ["January","February","March","April","May","June","July"],
+            "datasets":[{
+                "label": "High",
+                "data": [130,128,132,133,132,128,129],
+                "fill":false,
+                "borderColor": "red",
+                "lineTension":0.1
+            },{
+                "label": "Low",
+                "data": [65,59,80,81,56,55,40],
+                "fill":false,
+                "borderColor": "blue",
+                "lineTension": 0.1
+            }]
+        }
+    });
