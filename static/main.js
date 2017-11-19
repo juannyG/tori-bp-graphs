38b63d58a83262bca6916f38af2b64bff22efc3f
Juan Gutierrez <juanny.gee@gmail.com>
2017-11-19 18:41:22 -0500

    Add interstitial modal messagse

--- static/main.js
+++ static/main.js
@@ -1,14 +1,28 @@
 (function () {
+    var spinnerModal, doneModal;
+
     function setTimestamp() {
         var ts = new Date();
         $('#timestampInput').val(ts.toDateString());
     }
 
+    function initModals() {
+        spinnerModal = UIkit.modal($('#spinnerModal'));
+        doneModal = UIkit.modal($('#doneModal'));
+    }
+
     function createSuccess() {
-        location.reload();
+        doneModal.show();
+        setTimeout(doneModal.hide, 2000);
+    }
+
+    function showSpinnerModal() {
+        spinnerModal.show();
+        setTimeout(createSuccess, 2000);
     }
 
     function createBPEntry() {
+        showSpinnerModal();
         var high = $('#highInput').val();
         var low = $('#lowInput').val();
         var ts = new Date();
@@ -22,6 +36,7 @@
 
     $(document).ready(function(){
         setTimestamp();
+        initModals();
         $('#createBPButton').click(createBPEntry);
     });
 })();
